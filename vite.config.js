d331a007b8b8f7e0d608b18886b4fa791b33e040
techfusionhubin <techfusionhub.in@gmail.com>
2025-10-31 12:46:24 +0530

    i

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/techfusionhub/' : '/',
+  base: "./",
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
